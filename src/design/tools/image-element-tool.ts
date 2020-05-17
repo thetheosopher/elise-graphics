c3b17e8314a4351c574dc17a523eebf5863c9c62
Michael McCloskey <mike@dev.proscape.com>
2020-05-16 18:08:21 -0600

    Add native image aspect support to image element tool.

--- src/design/tools/image-element-tool.ts
+++ src/design/tools/image-element-tool.ts
@@ -3,6 +3,7 @@ import { Point } from '../../core/point';
 import { Size } from '../../core/size';
 import { ImageElement } from '../../elements/image-element';
 import { DesignTool } from './design-tool';
+import { BitmapResource } from '../../resource/bitmap-resource';
 
 /**
  * Image element creation tool
@@ -11,6 +12,8 @@ export class ImageElementTool extends DesignTool {
     public point1?: Point;
     public source?: string;
     public imageElement?: ImageElement;
+
+    public nativeAspect?: number;
 
     public cancelled: boolean = false;
 
@@ -39,6 +42,20 @@ export class ImageElementTool extends DesignTool {
         this.isCreating = true;
     }
 
+    private getNewSize(deltaX: number, deltaY: number) {
+        if(this.nativeAspect != null && this.aspectLocked) {
+            if(deltaX > deltaY) {
+                return new Size(deltaX, deltaX / this.nativeAspect);
+            }
+            else {
+                return new Size(deltaY * this.nativeAspect, deltaY);
+            }
+        }
+        else {
+            return new Size(deltaX, deltaY);
+        }
+    }
+
     public mouseMove(args: MouseLocationArgs) {
         if (this.cancelled) {
             return;
@@ -52,7 +69,7 @@ export class ImageElementTool extends DesignTool {
         if (args.location.x < this.point1.x || args.location.y < this.point1.y) {
             return;
         }
-        this.imageElement.setSize(new Size(args.location.x - this.point1.x, args.location.y - this.point1.y));
+        this.imageElement.setSize(this.getNewSize(args.location.x - this.point1.x, args.location.y - this.point1.y));
         if (this.controller) {
             this.controller.invalidate();
         }
@@ -71,7 +88,7 @@ export class ImageElementTool extends DesignTool {
         if (args.location.x < this.point1.x || args.location.y < this.point1.y) {
             return;
         }
-        this.imageElement.setSize(new Size(args.location.x - this.point1.x, args.location.y - this.point1.y));
+        this.imageElement.setSize(this.getNewSize(args.location.x - this.point1.x, args.location.y - this.point1.y));
         if (this.controller) {
             this.controller.invalidate();
         }
@@ -94,10 +111,17 @@ export class ImageElementTool extends DesignTool {
     public setImageSource(source: string) {
         this.source = source;
         if (this.model) {
-            const resource = this.model.resourceManager.get(this.source);
+            const resource:BitmapResource | undefined = this.model.resourceManager.get(this.source);
             if (resource && !resource.available) {
                 this.model.resourceManager.register(this.source);
-                this.model.resourceManager.load();
+                this.model.resourceManager.load((result) => {
+                    if(result && resource.image) {
+                        this.nativeAspect = resource.image.naturalWidth / resource.image.naturalHeight;
+                    }
+                });
+            }
+            else if(resource !== undefined && resource.image) {
+                this.nativeAspect = resource.image.naturalWidth / resource.image.naturalHeight;
             }
         }
     }
